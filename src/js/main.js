84b8e604d1973e439ac1f019500e8daf5aa760b3
Đặng Thanh Tuấn <91366633+ketuan1@users.noreply.github.com>
2025-10-07 22:04:36 +0700

    update

--- src/js/main.js
+++ src/js/main.js
@@ -12,11 +12,12 @@ $(document).ready(function () {
 	buttonToTop();
 	CounterUps();
 	TabNav();
+	Register();
 });
 
 /*==================== Aos Init ====================*/
 setTimeout(() => {
-AOS.init({
+	AOS.init({
 		offset: 100,
 	});
 }, 1000);
@@ -26,31 +27,31 @@ function FAQ() {
 	const faqItems = document.querySelectorAll('.item-faq');
 
 	if (faqItems.length > 0) {
-	faqItems[0].classList.add('active');
-	const firstIcon = faqItems[0].querySelector('i');
-	if (firstIcon) {
-		firstIcon.classList.replace('fa-plus', 'fa-minus');
-	}
+		faqItems[0].classList.add('active');
+		const firstIcon = faqItems[0].querySelector('i');
+		if (firstIcon) {
+			firstIcon.classList.replace('fa-plus', 'fa-minus');
+		}
 	}
 
 	faqItems.forEach(item => {
-	const title = item.querySelector('.title');
-	title.addEventListener('click', function () {
-		faqItems.forEach(faq => {
-		if (faq !== item) {
-			faq.classList.remove('active');
-			faq.querySelector('i').classList.replace('fa-minus', 'fa-plus');
-		}
-		});
+		const title = item.querySelector('.title');
+		title.addEventListener('click', function () {
+			faqItems.forEach(faq => {
+				if (faq !== item) {
+					faq.classList.remove('active');
+					faq.querySelector('i').classList.replace('fa-minus', 'fa-plus');
+				}
+			});
 
-		item.classList.toggle('active');
-		const icon = item.querySelector('i');
-		if (item.classList.contains('active')) {
-		icon.classList.replace('fa-plus', 'fa-minus');
-		} else {
-		icon.classList.replace('fa-minus', 'fa-plus');
-		}
-	});
+			item.classList.toggle('active');
+			const icon = item.querySelector('i');
+			if (item.classList.contains('active')) {
+				icon.classList.replace('fa-plus', 'fa-minus');
+			} else {
+				icon.classList.replace('fa-minus', 'fa-plus');
+			}
+		});
 	});
 }
 
@@ -58,18 +59,18 @@ function FAQ() {
 
 function TabNav() {
 	$('.list-tab li:first-child').addClass('active');
-		$('.tab-content').hide();
-		$('.tab-content:first').show();
+	$('.tab-content').hide();
+	$('.tab-content:first').show();
 
-		$('.list-tab li').click(function(){
+	$('.list-tab li').click(function () {
 		$('.list-tab li').removeClass('active');
 		$(this).addClass('active');
 		$('.tab-content').hide();
-		
+
 		var activeTab = $(this).find('a').attr('href');
 		$(activeTab).fadeIn();
 		return false;
-		});
+	});
 }
 
 function CounterUps() {
@@ -88,6 +89,42 @@ function CounterUps() {
 				}
 				$this.text(currentNumber);
 			}, 16);
+		});
+	});
+}
+
+function Register() {
+	$(document).ready(function () {
+		$('[data-form="login"]').hide();
+		function toggleForms(targetTab) {
+			$('.list-tab-form ul li a').removeClass('active');
+			$(`.list-tab-form ul li a[data-tab="${targetTab}"]`).addClass('active');
+			if (targetTab === 'login') {
+				$('[data-form="register"]').fadeOut(300, function () {
+					$('[data-form="login"]').fadeIn(300);
+				});
+				$('.wrap-login .list-tab-form ul li a').removeClass('active');
+				$('.wrap-login .list-tab-form ul li a[data-tab="login"]').addClass('active');
+			} else {
+				$('[data-form="login"]').fadeOut(300, function () {
+					$('[data-form="register"]').fadeIn(300);
+				});
+				$('.wrap-register .list-tab-form ul li a').removeClass('active');
+				$('.wrap-register .list-tab-form ul li a[data-tab="register"]').addClass('active');
+			}
+		}
+		$('.list-tab-form ul li a').click(function (e) {
+			e.preventDefault();
+			const targetTab = $(this).data('tab');
+			toggleForms(targetTab);
+		});
+		$('.wrap-register .question-form a').click(function (e) {
+			e.preventDefault();
+			toggleForms('login');
+		});
+		$('.wrap-login .question-form a').click(function (e) {
+			e.preventDefault();
+			toggleForms('register');
 		});
 	});
 }
